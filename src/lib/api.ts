f96d740a2002817b935d94580c339fcbec5e6915
MadeSatya5 <madesatya505@gmail.com>
2025-09-27 19:25:25 +0700

    fix: minor typos

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -42,7 +42,7 @@ api.interceptors.request.use(function (config) {
         throw 'Api Context not found. You must call `setApiContext(context)` before calling api on server-side';
 
       const cookies = new Cookies(context.req?.headers.cookie);
-      token = cookies.get('conation');
+      token = cookies.get(''); // Change with your token name
     } else {
       token = getToken();
     }
