8c4db12f3539b9ed48c285e18c3e057681e1d243
MadeSatya5 <madesatya505@gmail.com>
2026-02-08 20:38:02 +0700

    feat: fix typography size

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -71,31 +71,31 @@ export default function Typography<T extends React.ElementType>({
         // *=============== Font Variants ==================
         [
           variant === "h1" && [
-            "text-2xl leading-tight sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight lg:text-7xl lg:leading-tight",
+            "text-3xl leading-tight sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight lg:text-7xl lg:leading-tight",
           ],
           variant === "h2" && [
-            "text-xl leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:leading-tight",
+            "text-2xl leading-tight sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:leading-tight",
           ],
           variant === "h3" && [
-            "text-lg leading-tight sm:text-3xl sm:leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight",
+            "text-xl leading-tight sm:text-3xl sm:leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight",
           ],
           variant === "h4" && [
-            "text-base leading-snug sm:text-2xl sm:leading-snug md:text-3xl md:leading-snug lg:text-4xl lg:leading-snug",
+            "text-lg leading-snug sm:text-2xl sm:leading-snug md:text-3xl md:leading-snug lg:text-4xl lg:leading-snug",
           ],
           variant === "h5" && [
-            "text-sm leading-snug sm:text-xl sm:leading-snug md:text-2xl md:leading-snug lg:text-3xl lg:leading-snug",
+            "text-base leading-snug sm:text-xl sm:leading-snug md:text-2xl md:leading-snug lg:text-3xl lg:leading-snug",
           ],
           variant === "h6" && [
-            "text-xs leading-normal sm:text-lg sm:leading-normal md:text-xl md:leading-normal lg:text-2xl lg:leading-normal",
+            "text-sm leading-normal sm:text-lg sm:leading-normal md:text-xl md:leading-normal lg:text-2xl lg:leading-normal",
           ],
           variant === "title" && [
-            "text-xs leading-normal sm:text-sm sm:leading-normal md:text-base md:leading-normal lg:text-lg lg:leading-normal",
+            "text-sm leading-normal sm:text-base sm:leading-normal md:text-lg md:leading-normal lg:text-xl lg:leading-normal",
           ],
           variant === "body" && [
-            "text-xs leading-relaxed sm:text-sm sm:leading-relaxed md:text-base md:leading-relaxed",
+            "text-sm leading-relaxed sm:text-base sm:leading-relaxed",
           ],
           variant === "label" && [
-            "text-[10px] leading-normal sm:text-xs sm:leading-normal md:text-sm md:leading-normal",
+            "text-xs leading-normal sm:text-sm sm:leading-normal",
           ],
         ],
         className
